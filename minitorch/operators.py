035f572318c0caa48a4f1b9eebd084d4165ee49e
Arad Ganir <aradganir@Arads-MacBook-Pro-4.local>
2025-01-10 13:57:19 -0800

    First commit

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,7 +3,7 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
+
 
 #
 # Implementation of a prelude of elementary functions.
@@ -33,6 +33,111 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(x, y):
+    """multiply.
+
+    Args:
+    ----
+        x (int): The first argument.
+        y (int): The second argument.
+
+    Returns:
+    -------
+        int: The result of multiplying x and y.
+
+    """
+    return x * y
+
+
+def id(x):
+    """Returns argument x"""
+    return x
+
+
+def add(x, y):
+    """Adds two arguments, x, y, returns x+y"""
+    return x + y
+
+
+def sub(x, y):
+    """Subtracts two arguments, x, y, returns x-y"""
+    return x - y
+
+
+def neg(x):
+    """Negates argument, returns -x"""
+    return -x
+
+
+def lt(x, y):
+    """Returns True if x is less than y, returns False otherwise."""
+    return x < y
+
+
+def eq(x, y):
+    """Returns True if x is equal to y, returns False otherwise."""
+    return x == y
+
+
+def max(x, y):
+    """Returns the maximum value of x and y."""
+    return x if x > y else y
+
+
+def is_close(input, other):
+    """Takes input, other, and returns true if input is within .01 of other."""
+    return abs(input - other) <= 1e-2
+
+
+def sigmoid(x):
+    """Implements sigmoid function symmetrically"""
+    if x >= 0:
+        return 1 / (1 + math.exp(-x))
+    else:
+        return math.exp(x) / (1 + math.exp(x))
+
+
+def relu(x):
+    """Implements the relu function"""
+    return max(0, x)
+
+
+def log(x):
+    """Renames the math.log function"""
+    return math.log(x)
+
+
+def exp(x):
+    """Renames the math.exp function"""
+    return math.exp(x)
+
+
+def inv(x):
+    """Returns reciprocal"""
+    return 1 / x
+
+
+def log_back(a, b):
+    """Returns the derivative of a function 'a', and multiplies by value of b"""
+    h = 1e-8
+    return b * inv(a)
+
+
+def inv_back(a, b):
+    """Returns the derivative of inverse function, multiplies by b"""
+    h = 1e-8
+    return -b * (inv(a) ** 2)
+
+
+def relu_back(a, b):
+    """Returns the value of the derivative of relu multiplied by b.
+    If over 0, returns b otherwise returns 0
+    """
+    h = 1e-8
+    if a > 0:
+        return b
+    else:
+        return 0
 
 
 # ## Task 0.3
@@ -49,6 +154,34 @@ from typing import Callable, Iterable
 # - addLists : add two lists together
 # - sum: sum lists
 # - prod: take the product of lists
+
+
+def map():
+    pass
+
+
+def zipWith():
+    pass
+
+
+def reduce():
+    pass
+
+
+def negList():
+    pass
+
+
+def addLists():
+    pass
+
+
+def sum():
+    pass
+
+
+def prod():
+    pass
 
 
 # TODO: Implement for Task 0.3.
